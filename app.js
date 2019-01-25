6c711cec918f0cf933803178f74a1d451ee5b43d
juhui xue <xuejuhui@hotmail.com>
2019-01-24 19:58:51 -0500

    added confetti to the page

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    console.log('loaded')
+document.addEventListener('DOMContentLoaded', () => {
+
+    (function frame() {
+        // launch a few confetti from the left edge
+        confetti({
+            particleCount: 7,
+            angle: 180,
+            spread: 360,
+            origin: { x: 0.8, y:0 }
+        });
+        // and launch a few from the right edge
+        confetti({
+            particleCount: 7,
+            angle: 360,
+            spread: 360,
+            origin: { x: 0.2, y:0 }
+        });
+
+        // keep going
+        if (Date.now()) {
+            requestAnimationFrame(frame);
+        }
+    }());
 })
